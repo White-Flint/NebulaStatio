a117b1839160933b7146ac6b86e4d0b33ebf8f6a
Tien Young <constantine.sky@hotmail.com>
2025-06-11 23:22:21 -0400

    Combine blazorwasm and web api into blazor web app

--- Nebula/Program.cs
+++ Nebula/Program.cs
@@ -1,18 +1,14 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace Nebula;
-
-public class Program
+namespace Nebula
 {
-    public static async Task Main(string[] args)
+    internal class Program
     {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-        await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
     }
 }
